c7230487eaed73df4ff0b908cdf8f672ba09ed0c
Dylan Marsh <dylantmarsh@Mac.localdomain>
2024-12-28 18:07:45 -0500

    Adding FlowBite component library

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { Navbar, NavBrand, Button } from 'flowbite-svelte';
+  import { MoonOutline } from 'flowbite-svelte-icons';
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      class="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Screen Sentry
+    </span>
+  </NavBrand>
+
+  <Button color="purple" pill={true} class="!p-2">
+    <MoonOutline class="w-5 h-5" />
+  </Button>
+</Navbar>
